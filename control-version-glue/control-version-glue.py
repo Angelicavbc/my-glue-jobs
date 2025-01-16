a0afbb8b7b0761eea212e9c2bca328bfa15f4815
Angelicavbc <116378419+Angelicavbc@users.noreply.github.com>
2025-01-16 17:11:46 -0500

    Synchronizing job control-version-glue from AWS Glue at 2025/01/16 22:11:46

---
+++ control-version-glue/control-version-glue.py
@@ -0,0 +1,23 @@
+import sys
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+# Inicialización
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+# Leer datos desde S3
+input_path = "s3://test-version-control-glue/versionControl.csv"  # Reemplaza con el bucket y carpeta que ya tengas
+df = spark.read.format("csv").option("header", "true").load(input_path) #Funciono el control version
+
+# Imprimir los datos en el log
+df.show()
+
+# Finalizar el job
+job.commit()
